f0bbf155682029a419a24fd025fa9db352325321
Sunil <iamsunilsheoran4590@gmail.com>
2025-03-19 18:47:03 +0530

    Html

---
+++ script.js
@@ -0,0 +1,17 @@
+```javascript
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+}
+
+function calculateResult() {
+    const display = document.getElementById('display');
+    try {
+        display.value = eval(display.value);
+    } catch {
+        display.value = 'Error';
+    }
+}
